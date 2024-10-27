13dc529a7b00310e525800e0f44d5ec67352d578
joao.jo.rj@gmail.com <81653692+Grandogs@users.noreply.github.com>
2024-10-26 23:19:12 -0300

    update the task

--- tarefa_trainees.py
+++ tarefa_trainees.py
@@ -1,18 +1,18 @@
 from openpyxl import Workbook
 import pandas as pd
 
+# ler arquivo
+df_origem = pd.read_excel(caminho_origem)
+
 # Caminhos dos arquivos
 caminho_origem = 'transacoes_financeiro_completo.xlsx'
 caminho_destino = 'transacoes_financeiro_1_semestre.xlsx'
 
-
-df_origem = pd.read_excel(caminho_origem)
-
-# Verificar e selecionar as colunas necessárias
+# Selecionar as colunas
 colunas_necessarias = ['Data', 'Valor (R$)', 'Nota Fiscal', 'Categoria', 'Tipo de Transação']
 df_origem = df_origem[colunas_necessarias]
 
-# Exporta os dados para a planilha de destino
+# Exporta os dados para a planilha
 df_origem.to_excel(caminho_destino, index=False)
 
-print("Planilha atualizada com sucesso!")
+print("Feito!")
